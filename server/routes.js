f413ef7ad65db56c252ffedceca279e53ac17d42
FernandaRon <rondon.fernanda11@gmail.com>
2019-04-24 14:45:38 -0500

    fixed routes

--- server/routes.js
+++ server/routes.js
@@ -1,41 +1,75 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 // UNCOMMENT THE DATABASE YOU'D LIKE TO USE
-var db = require('../database');
+var db = require('../database/mysql.js');
 // var items = require('../database-mongo');
 var app = express();
 // UNCOMMENT FOR REACT
 app.use(express.static(__dirname + '/../react-client/dist'));
 
-
-
-app.get ('/home',function (req, res) =>{
-  db.getStyles(data => {
-    res.status(200).send(data)
-  })
-});
-
-app.get  ('/style', function (req,res) => {
-  Promise.all([
-    db.getInfoByStyle(),
-    db.imagesByStyle()
-  ])
-    .then (data => {
+//Query dB for StylesDropdown
+exports.getStyles = (req, res) => {
+  db.getStyles()
+    .then(data => {
       res.status(200).send(data)
     })
-    .catch (err => {
-      console.log err;
+    .catch(err => { console.log(err)})
+}
+
+//Qyuery dB for Images of the Style
+exports.getInfoByStyle = (req, res) => {
+  db.getInfoByStyle()
+    .then(data => {
+      res.status(200).send(data)
     })
-})
+    .catch(err => { console.log(err)})
+}
 
-app.post ('/login',function (req, res) => {
-  db.getUser(data => {
-    res.status(200).send(data)
-  })
-});
+exports.getUser = (req, res) => {
+  db.getUser()
+    .then(data => {
+      res.status(200).send(data)
+    })
+    .catch(err => { console.log(err)})
+}
 
-app.post ('/login',function (req, res) => {
-  db.getApp(data => {
-    res.status(200).send(data)
-  })
-});
+
+exports.getApp = (req, res) => {
+  db.getApp()
+    .then(data => {
+      res.status(200).send(data)
+    })
+    .catch(err => { console.log(err)})
+}
+
+
+// app.get ('/home',function (req, res) =>{
+//   db.getStyles(data => {
+//     res.status(200).send(data)
+//   })
+// });
+//
+// app.get  ('/style', function (req,res) => {
+//   Promise.all([
+//     db.getInfoByStyle(),
+//     db.imagesByStyle()
+//   ])
+//     .then (data => {
+//       res.status(200).send(data)
+//     })
+//     .catch (err => {
+//       console.log err;
+//     })
+// })
+//
+// app.post ('/login',function (req, res) => {
+//   db.getUser(data => {
+//     res.status(200).send(data)
+//   })
+// });
+//
+// app.post ('/login',function (req, res) => {
+//   db.getApp(data => {
+//     res.status(200).send(data)
+//   })
+// });
