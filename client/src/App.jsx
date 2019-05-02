61db7ddf66d76347cc1cda5fa91e44370f613e6e
gersonbarajas62 <gersonbarajas62@gmail.com>
2019-05-02 12:22:34 -0500

    working on auth component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,56 +10,8 @@ import axios from 'axios';
 class App extends Component {
   constructor(props) {
     super(props);
-      this.state = {
-        styles : [],
-        style: {},
-        
-
-    };
-    this.selectStyles = this.selectStyles.bind(this)
-   
+    
   }
-
-  componentDidMount(){
-    axios.get('/home')
-    .then(res => {
-      console.log("this is my res:", res.data)
-      this.setState({
-        styles : res.data,
-        })
-
-    })
-    .catch(err => {
-      console.log("this is my err:", err);
-    });
-
-    axios.get('/home-images')
-    .then(res => {
-      console.log("this is my res:", res.data)
-      this.setState({
-        images : res.data,
-        })
-
-    })
-    .catch(err => {
-      console.log("this is my err:", err);
-    })
-
-
-
- }
-
- selectStyles(id) {
-     const { styles } = this.state;
-     console.log(typeof styles[0].id)
-     console.log(typeof id)
-     const style = styles.filter(style => +id === style.id);
-     this.setState({
-       style: style[0]
-    })
- }
-
-
 
   render() {
     return (
@@ -70,16 +22,16 @@ class App extends Component {
 
 
             <Switch>
-              <Route exact path="/" render={(props) => <Home {...props} data={this.state.styles} selectStyles={this.selectStyles} style={this.state.style}/>} />
+              <Route exact path="/" render={(props) => <Home {...props} />
+            } />
               <Route path="/book" component={Book} />
             </Switch>
         </div>
+        
       </BrowserRouter>
-
     )
   }
 }
-
 // hot export works with RHL. Remove line 11 when starting fullstack integration
 export default App;
 // Uncomment line 13 & delete line 11 when starting fullstack integration
