485aa74f53b2ef01e14601d5c88034b962a4c822
YordaniBonilla <b.yordani93@gmail.com>
2019-05-02 05:33:20 -0500

    update our MapContainer component to include our new changes

--- client/src/Components/Map.js
+++ client/src/Components/Map.js
@@ -43,6 +43,79 @@ export class CurrentLocation extends React.Component {
           map.panTo(center);
         }
       }
+
+      componentDidMount() {
+        if (this.props.centerAroundCurrentLocation) {
+          if (navigator && navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(pos => {
+              const coords = pos.coords;
+              this.setState({
+                currentLocation: {
+                  lat: coords.latitude,
+                  lng: coords.longitude
+                }
+              });
+            });
+          }
+        }
+        this.loadMap();
+      }
+
+      loadMap() {
+        if (this.props && this.props.google) {
+          // checks if google is available
+          const { google } = this.props;
+          const maps = google.maps;
+    
+          const mapRef = this.refs.map;
+    
+          // reference to the actual DOM element
+          const node = ReactDOM.findDOMNode(mapRef);
+    
+          let { zoom } = this.props;
+          const { lat, lng } = this.state.currentLocation;
+          const center = new maps.LatLng(lat, lng);
+          const mapConfig = Object.assign(
+            {},
+            {
+              center: center,
+              zoom: zoom
+            }
+          );
+    
+          // maps.Map() is constructor that instantiates the map
+          this.map = new maps.Map(node, mapConfig);
+        }
+      }
+
+      renderChildren() {
+        const { children } = this.props;
+    
+        if (!children) return;
+    
+        return React.Children.map(children, c => {
+          if (!c) return;
+          return React.cloneElement(c, {
+            map: this.map,
+            google: this.props.google,
+            mapCenter: this.state.currentLocation
+          });
+        });
+      }
+
+      render() {
+        const style = Object.assign({}, mapStyles.map);
+       return (
+         <div>
+           <div style={style} ref="map">
+             Loading map...
+           </div>
+           {this.renderChildren()}
+         </div>
+       );
+     }
+   
+    
     
     
 
--- client/src/Components/MapContainer.jsx
+++ client/src/Components/MapContainer.jsx
@@ -1,23 +1,48 @@
 import React, { Component } from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
 
-const mapStyles = {
-  width: '100%',
-  height: '100%'
-};
+import CurrentLocation from './Map';
 
 export class MapContainer extends Component {
+  state = {
+    showingInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {}
+  };
+
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+
+  onClose = props => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  };
+
   render() {
     return (
-      <Map
+      <CurrentLocation
+        centerAroundCurrentLocation
         google={this.props.google}
-        zoom={14}
-        style={mapStyles}
-        initialCenter={{
-         lat: -1.2884,
-         lng: 36.8233
-        }}
-      />
+      >
+        <Marker onClick={this.onMarkerClick} name={'current location'} />
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+          onClose={this.onClose}
+        >
+          <div>
+            <h4>{this.state.selectedPlace.name}</h4>
+          </div>
+        </InfoWindow>
+      </CurrentLocation>
     );
   }
 }
