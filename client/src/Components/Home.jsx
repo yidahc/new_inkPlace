46bb4afc2140e7532634fb735f548b539bb72e85
YordaniBonilla <b.yordani93@gmail.com>
2019-05-03 07:25:27 -0500

    wrapped Login/Register in proper syntax in Home Component

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -15,10 +15,10 @@ class Home extends React.Component {
         
           <div className="box-controller">
           <div className="controller">
-            Login
+            <Login/>
           </div>
           <div className="controller">
-            register
+            <Register/>
           </div>
           </div>
           <div className="box-container">
@@ -28,69 +28,7 @@ class Home extends React.Component {
         )}
     }
 
-    class LoginBox extends React.Component {
-      constructor(props) {
-        super(props);
-        this.state = {  };
-      }
+
+
+
      
-      render() {
-        return(
-          <div className="inner-container">
-           <div className="header">
-          Login
-          </div>
-          <div className="box">
-          <div className="input-group">
-          <label htmlFor="userName"> username</label>
-          <input type="text" name="username" className="login-input" placeholder="username"/>
-          </div>
-          
-          <div className="input-group">
-          <label htmlFor="password">password</label>
-          <input type="text" name="password" classNAme="login-input" placeholder="password"/>
-          </div>
-          <button type="button" className="login-btn" onClick={this.submitLogin.bind(this)}>Login</button>
-          </div>
-
-          </div>
-        )
-      }
-    }
-
-
-
-
-     class Register extends  React.Component {
-      constructor(props) {
-        super(props);
-        this.state = {};
-      }
-      submitRegister(e) {
-        
-      }
-      render() {
-        return(
-          <div className="inner-container">
-          <div className="header">
-          Register
-          </div>
-          <div className="box">
-          <div className="input-group">
-          <label htmlFor="userName">username</label>
-          <input type="text" name="username" placeholder="username"/>
-          </div>
-          
-          <div className="input-group">
-          <label htmlFor="password"> password</label>
-          <input type="text" name="password" placeholder="passweord"/>
-          </div>
-          <button type="button" className="login-btn" onClick={this.submitRegister.bind(this)}>Register</button>
-          </div>
-
-          </div>
-        )
-      }
-    }
-
-export default Home;
