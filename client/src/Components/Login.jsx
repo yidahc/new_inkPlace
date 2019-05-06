2fe0261361e8edc5e2e3c846f5d0f10aa236c6fe
gersonbarajas62 <gersonbarajas62@gmail.com>
2019-05-06 12:48:31 -0500

    working in the component

--- client/src/Components/Login.jsx
+++ client/src/Components/Login.jsx
@@ -3,28 +3,45 @@ import ReactDom from 'react-dom'
 import LoginBox from './LoginBox'
 import RegisterBox from './RegisterBox'
 
+
+
 class Login extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       isLoginOpen: true,
-      isRegisterOpen: true
+      isRegisterOpen: false
     };
   }
+  showLoginBox() {
+   this.setState({isLoginOpen: true, isRegisterOpen: false})
+  }
+  showRegisterBox() {
+    this.setState({isRegisterOpen: true, isLoginOpen: false })
+  };
 
+  
   render() {
 
     return (
       <div>
       <div className="root-container">
+      <div className="controller" onClick={this.showLoginBox.bind(this)} >
+      Login
+      </div>
+      <div className="controller" onClick={this.showRegisterBox.bind(this)}>
+      Register
+      </div>
+      </div>
+      <div className="box-container">
         {this.state.isLoginOpen && <LoginBox/>}
-        </div>
-        <div>
         {this.state.isRegisterOpen && <RegisterBox/>}
         </div>
         </div>
     );
   }
 }
+
+
 
 export default Login;
