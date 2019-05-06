05ce8ed146baf4ef3433b0ceb89e87e670098f6c
YordaniBonilla <b.yordani93@gmail.com>
2019-05-06 11:23:55 -0500

    add styles button

--- client/src/Components/Navigation.jsx
+++ client/src/Components/Navigation.jsx
@@ -7,14 +7,18 @@ const Navigation = () => {
   return  (
     <div className="bg-info clearfix">
       <div className="btn  float-left">
-      <NavLink to="/"><Button color="danger" >Home</Button></NavLink>
+        <NavLink to="/"><Button color="danger" >Home</Button></NavLink>
       </div>
       <div  className="btn float-right">
         <NavLink to="/login"><Button color="danger">Login</Button></NavLink>
       </div>
       <div className="btn float-right">
-      <NavLink to="/map"><Button color="danger">Map</Button></NavLink>
+        <NavLink to="/map"><Button color="danger">Map</Button></NavLink>
       </div>  
+
+      <div className="btn  float-right">
+        <NavLink to="/styles"><Button color="danger">Styles</Button></NavLink>
+      </div>
       
     </div>
 
